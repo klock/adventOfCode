3907f449314e0a22842255f07ce6c7b132c79348
Math Klock <math.klock@gmail.com>
2020-12-03 08:08:46 +0100

    Day 3 Part 1 - ride slope repeated 2 times

--- src/main/java/fr/klock/dev/training/adventOfCode/year2020/day3/Toboggan.java
+++ src/main/java/fr/klock/dev/training/adventOfCode/year2020/day3/Toboggan.java
@@ -1,5 +1,36 @@
 package fr.klock.dev.training.adventOfCode.year2020.day3;
 
+import fr.klock.dev.training.adventOfCode.MatrixUtils;
+
 public class Toboggan {
 
+    private final char[][] matrix;
+
+    public Toboggan(char[][] matrix) {
+        this.matrix = matrix;
+    }
+
+    public static Toboggan initFrom(char[][] matrix) {
+        return new Toboggan(MatrixUtils.copyMatrix(matrix));
+    }
+
+    public char[][] rideSlope(int downMove, int rightMove) {
+        int[] pos = new int[]{downMove, rightMove};
+        while (pos[0] < matrix.length) {
+            ride(this.matrix[pos[0]], pos[1]);
+            // increment
+            pos[0] = pos[0] + downMove;
+            pos[1] = pos[1] + rightMove;
+        }
+        return MatrixUtils.copyMatrix(matrix);
+    }
+
+    private void ride(char[] line, int i) {
+        char step = line[i];
+        if (step == '.') {
+            line[i] = 'O';
+        } else if (step == '#') {
+            line[i] = 'X';
+        }
+    }
 }
--- src/test/java/fr/klock/dev/training/adventOfCode/year2020/day3/TobogganTest.java
+++ src/test/java/fr/klock/dev/training/adventOfCode/year2020/day3/TobogganTest.java
@@ -1,7 +1,9 @@
 package fr.klock.dev.training.adventOfCode.year2020.day3;
 
+import fr.klock.dev.training.adventOfCode.MatrixUtils;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -10,5 +12,47 @@ import static org.assertj.core.api.Assertions.assertThat;
 @TestMethodOrder(MethodOrderer.Alphanumeric.class)
 public class TobogganTest {
 
+    private final static String PATTERN =
+            "..##.......\n" +
+            "#...#...#..\n" +
+            ".#....#..#.\n" +
+            "..#.#...#.#\n" +
+            ".#...##..#.\n" +
+            "..#.##.....\n" +
+            ".#.#.#....#\n" +
+            ".#........#\n" +
+            "#.##...#...\n" +
+            "#...##....#\n" +
+            ".#..#...#.#";
 
+
+    @Test
+    @DisplayName("Ride slope for pattern copied 2 times")
+    void rideSlope() {
+        Toboggan toboggan =  Toboggan.initFrom(MatrixUtils.repeatPattern(MatrixUtils.readFromPattern(PATTERN), 2));
+
+        char[][] riddenMatrix = toboggan.rideSlope(1, 3);
+
+        char[][] expectedPattern = new char[][]{
+                {'.', '.', '#', '#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#', '#', '.', '.', '.', '.', '.', '.', '.', '.', '.', '#', '#', '.', '.', '.', '.', '.', '.', '.'},
+                {'#', '.', '.', 'O', '#', '.', '.', '.', '#', '.', '.', '#', '.', '.', '.', '#', '.', '.', '.', '#', '.', '.', '#', '.', '.', '.', '#', '.', '.', '.', '#', '.', '.'},
+                {'.', '#', '.', '.', '.', '.', 'X', '.', '.', '#', '.', '.', '#', '.', '.', '.', '.', '#', '.', '.', '#', '.', '.', '#', '.', '.', '.', '.', '#', '.', '.', '#', '.'},
+                {'.', '.', '#', '.', '#', '.', '.', '.', '#', 'O', '#', '.', '.', '#', '.', '#', '.', '.', '.', '#', '.', '#', '.', '.', '#', '.', '#', '.', '.', '.', '#', '.', '#'},
+                {'.', '#', '.', '.', '.', '#', '#', '.', '.', '#', '.', '.', 'X', '.', '.', '.', '#', '#', '.', '.', '#', '.', '.', '#', '.', '.', '.', '#', '#', '.', '.', '#', '.'},
+                {'.', '.', '#', '.', '#', '#', '.', '.', '.', '.', '.', '.', '.', '#', '.', 'X', '#', '.', '.', '.', '.', '.', '.', '.', '#', '.', '#', '#', '.', '.', '.', '.', '.'},
+                {'.', '#', '.', '#', '.', '#', '.', '.', '.', '.', '#', '.', '#', '.', '#', '.', '#', '.', 'O', '.', '.', '#', '.', '#', '.', '#', '.', '#', '.', '.', '.', '.', '#'},
+                {'.', '#', '.', '.', '.', '.', '.', '.', '.', '.', '#', '.', '#', '.', '.', '.', '.', '.', '.', '.', '.', 'X', '.', '#', '.', '.', '.', '.', '.', '.', '.', '.', '#'},
+                {'#', '.', '#', '#', '.', '.', '.', '#', '.', '.', '.', '#', '.', '#', '#', '.', '.', '.', '#', '.', '.', '.', '#', '.', 'X', '#', '.', '.', '.', '#', '.', '.', '.'},
+                {'#', '.', '.', '.', '#', '#', '.', '.', '.', '.', '#', '#', '.', '.', '.', '#', '#', '.', '.', '.', '.', '#', '#', '.', '.', '.', '#', 'X', '.', '.', '.', '.', '#'},
+                {'.', '#', '.', '.', '#', '.', '.', '.', '#', '.', '#', '.', '#', '.', '.', '#', '.', '.', '.', '#', '.', '#', '.', '#', '.', '.', '#', '.', '.', '.', 'X', '.', '#'}
+        };
+
+        assertThat(riddenMatrix.length).isEqualTo(expectedPattern.length);
+        for (int i = 0; i < riddenMatrix.length; i++) {
+            assertThat(riddenMatrix[i].length).isEqualTo(expectedPattern[i].length);
+            for (int j = 0; j < riddenMatrix[i].length; j++) {
+                assertThat(riddenMatrix[i][j]).isEqualTo(expectedPattern[i][j]);
+            }
+        }
+    }
 }
