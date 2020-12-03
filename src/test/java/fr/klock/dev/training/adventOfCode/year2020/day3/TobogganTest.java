7046700e014227c345ca36ebb854ed5011999082
Math Klock <math.klock@gmail.com>
2020-12-03 09:30:07 +0100

    Day 3 Part 2 - product of ride tries encountered trees

--- src/test/java/fr/klock/dev/training/adventOfCode/year2020/day3/TobogganTest.java
+++ src/test/java/fr/klock/dev/training/adventOfCode/year2020/day3/TobogganTest.java
@@ -60,27 +60,66 @@ public class TobogganTest {
     }
 
     @Test
-    void getNumberOfEncounteredTrees() {
-        Toboggan toboggan =  Toboggan.initFrom(MatrixUtils.repeatPattern(MatrixUtils.readFromPattern(PATTERN), 2));
-
-        toboggan.rideSlope(1, 3);
-
-        int numberOfEncounteredTrees = toboggan.getNumberOfEncounteredTrees();
-
+    void getNumberOfEncounteredTreesRide_3_1() {
+        int numberOfEncounteredTrees = rideSlopeForDemoPattern(3, 1);
         assertThat(numberOfEncounteredTrees).isEqualTo(7);
     }
 
     @Test
-    void testPuzzleInputPartOne() throws IOException {
-        String[] slopeLines = FileUtils.loadInput(this, "slope.txt");
+    void getProductForNumberOfEncounteredTrees() {
+        int ride1_1 = rideSlopeForDemoPattern(1, 1);
+        int ride3_1 = rideSlopeForDemoPattern(3, 1);
+        int ride5_1 = rideSlopeForDemoPattern(5, 1);
+        int ride7_1 = rideSlopeForDemoPattern(7, 1);
+        int ride1_2 = rideSlopeForDemoPattern(1, 2);
+        assertThat(ride1_1).isEqualTo(2);
+        assertThat(ride3_1).isEqualTo(7);
+        assertThat(ride5_1).isEqualTo(3);
+        assertThat(ride7_1).isEqualTo(4);
+        assertThat(ride1_2).isEqualTo(2);
+        int product = ride1_1 * ride3_1 * ride5_1 * ride7_1 * ride1_2;
+        assertThat(product).isEqualTo(336);
+    }
 
-        Toboggan toboggan = Toboggan.initFrom(MatrixUtils.repeatPattern(MatrixUtils.readFromPattern(slopeLines), 500));
+    private int rideSlopeForDemoPattern(int rightMove, int downMove) {
+        Toboggan toboggan =
+                Toboggan.initFrom(
+                        MatrixUtils.repeatPattern(
+                                MatrixUtils.readFromPattern(PATTERN),
+                                7));
 
-        toboggan.rideSlope(1, 3);
+        toboggan.rideSlope(downMove, rightMove);
 
-        int numberOfEncounteredTrees = toboggan.getNumberOfEncounteredTrees();
+        return toboggan.getNumberOfEncounteredTrees();
+    }
 
+    @Test
+    void numberOfTreesForPuzzleInputRide_3_1() throws IOException {
+        int numberOfEncounteredTrees = rideSlopeOfPuzzleInput(3, 1);
         assertThat(numberOfEncounteredTrees).isEqualTo(232);
     }
 
+    @Test
+    void getProductForNumberOfEncounteredTreesForPuzzleInput() throws IOException {
+        int ride1_1 = rideSlopeOfPuzzleInput(1, 1);
+        int ride3_1 = rideSlopeOfPuzzleInput(3, 1);
+        int ride5_1 = rideSlopeOfPuzzleInput(5, 1);
+        int ride7_1 = rideSlopeOfPuzzleInput(7, 1);
+        int ride1_2 = rideSlopeOfPuzzleInput(1, 2);
+        long product = (long) ride1_1 * ride3_1 * ride5_1 * ride7_1 * ride1_2;
+        assertThat(product).isEqualTo(3952291680L);
+    }
+
+    private int rideSlopeOfPuzzleInput(int rightMove, int downMove) throws IOException {
+        Toboggan toboggan =
+                Toboggan.initFrom(
+                        MatrixUtils.repeatPattern(
+                                MatrixUtils.readFromPattern(
+                                        FileUtils.loadInput(this, "slope.txt"))
+                                , 75));
+
+        toboggan.rideSlope(downMove, rightMove);
+
+        return toboggan.getNumberOfEncounteredTrees();
+    }
 }
